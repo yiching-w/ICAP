afbb4e6ae0a2c6ef9027aafd88eff6d836fd8210
yiching <doo8409255@gmail.com>
2022-05-09 14:55:28 +0800

    v6

--- script/script.js
+++ script/script.js
@@ -1,11 +1,13 @@
 const scrollTop = $('.scrollTop');
 $(window).scroll(function() {
-    var topPos = $(this).scrollTop();
+    let topPos = $(this).scrollTop();
     if (topPos > 100) {
-        $(scrollTop).css('opacity', '0.8');
+        $(scrollTop).css('opacity', '1');
+        $(scrollTop).css('display', 'flex');
 
     } else {
         $(scrollTop).css('opacity', '0');
+        $(scrollTop).css('display', 'none');
     }
 
 });
@@ -91,3 +93,30 @@ items.forEach((item) => {
         }
     });
 });
+
+/*==================== SHOW NAVBAR ====================*/
+const showMenu = (headerToggle, navbarId) => {
+    const toggleBtn = document.getElementById(headerToggle),
+        nav = document.getElementById(navbarId)
+
+    // Validate that variables exist
+    if (headerToggle && navbarId) {
+        toggleBtn.addEventListener('click', () => {
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+                // change icon
+            toggleBtn.classList.toggle('bx-x')
+        })
+    }
+}
+showMenu('header-toggle', 'navbar')
+
+/*==================== LINK ACTIVE ====================*/
+const linkColor = document.querySelectorAll('.nav__link')
+
+function colorLink() {
+    linkColor.forEach(l => l.classList.remove('active'))
+    this.classList.add('active')
+}
+
+linkColor.forEach(l => l.addEventListener('click', colorLink))
